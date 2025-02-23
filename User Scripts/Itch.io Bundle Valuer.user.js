027ccad7a699d3a628ed7600b9e3b5a2a9840b0f
Jeremy W. Sherman <jeremyw.sherman@gmail.com>
2025-02-23 10:29:34 -0500

    Reorder for easier pasting into console

--- User Scripts/Itch.io Bundle Valuer.user.js
+++ User Scripts/Itch.io Bundle Valuer.user.js
@@ -47,7 +47,7 @@ function addPrices(games) {
         addPricesObserver.disconnect();
     }
 }
-/*
+/**
 Possible price strings are:
 
 $3
@@ -57,12 +57,14 @@ $3
 R$15 (Brazilian reais)
 
 And probably more, but these are the ones I've seen so far.
+
+@returns {[symbol: string, amount: number]}
 */
-let priceEntry = (priceString) => {
+let priceEntry = (/** @type{string} */priceString) => {
   const symbol = priceString.replace(/[0-9.,]/g, "");
   const amount = Number.parseFloat(priceString.replace(/[^0-9.]/g, ""));
   //   console.log(`${priceString} => [${symbol}, ${amount}]`);
-  return [symbol, amount];
+  return ([symbol, amount]);
 };
 let symbolValues = {
     '$': 1,
@@ -86,47 +88,9 @@ function stickAmountOnPage(msg) {
     strong.textContent = msg
     const p = document.createElement('p');
     p.appendChild(strong);
-    parent.appendChild(p);
+    parent?.appendChild(p);
 }
-async function computeBundleValue() {
-    console.log(`JWS: Computing bundle value`)
-    let loc = gamesUrl(document.location)
-    if (!loc) {
-        console.error('JWS: no game URL returned');
-        return;
-    }
-
-    console.log(`JWS: Fetching games.json from ${loc}`)
-    const res = await fetch(loc, { credentials: 'same-origin' })
-    if (!res.ok) {
-        console.error('JWS: failed fetching games.json')
-        return
-    }
-    const {games} = await res.json()
-    addPrices(games);
-  const unownedGames = games.filter((it) => !ownedGameIds.has(it.id));
-  console.log(
-    `JWS: Found ${games.length} total games, ${unownedGames.length} unowned games.`
-  );
-  const value = unownedGames.reduce((accu, game) => {
-    try {
-      if (game.flag === "free" || game.flag === "web") {
-        return accu;
-      }
-      const [key, value] = priceEntry(game.price);
-      if (!accu[key]) {
-        console.log(
-          `New currency symbol: ${key} - from price string: ${game.price}`
-        );
-        accu[key] = 0;
-      }
-      accu[key] += value;
-      //   console.log(`Now at ${key}${accu[key]}`);
-    } catch (error) {
-      console.error(`Failed to price a game.`, { game, error });
-    }
-    return accu;
-  }, {});
+function generateValueMessage(value, unownedGameCount) {
   const formatters = Object.fromEntries(
     Object.keys(value).map((currencySign) => [
       currencySign,
@@ -164,10 +128,83 @@ async function computeBundleValue() {
   });
   const countFmt = new Intl.NumberFormat("en-US");
   const msg = `${countFmt.format(
-    unownedGames.length
+    unownedGameCount
   )} new games. Approximate total value in USD: ${fmt.format(approx)}${
     stringified ? `, by way of ${stringified}` : ""
   }.`;
+  return msg;
+}
+/** Given on a sales bundle page, meaning URL path beginning /s/, computes the value of the unowned items. */
+async function computeSalesBundleValue() {
+  /** @type{HTMLElement[]} */
+  const gamedivs = Array.from(
+    document.querySelectorAll(".sale_page .game_grid_widget div[data-game_id]")
+  );
+  const unownedGameDivs = gamedivs.filter(
+    (it) => !ownedGameIds.has(Number.parseInt(it.dataset.gameId ?? ""))
+  );
+  const unownedGamePrices = unownedGameDivs
+    .map((gameDiv) =>
+      gameDiv?.querySelector("a.price_tag .price_value")?.textContent?.trim() ?? ''
+    )
+    .filter((it) => it.length > 0);
+  const value = unownedGamePrices.reduce((accu, priceString) => {
+    /* TODO: What about Web and Free items in a sale? */
+    const [key, value] = priceEntry(priceString);
+      if (!accu[key]) {
+        console.log(
+          `New currency symbol: ${key} - from price string: ${priceString}`
+        );
+        accu[key] = 0;
+      }
+      accu[key] += value;
+      return accu
+  }, {})
+  /* TODO: Synthesize price and plop on page. */
+}
+async function computeBundleValue() {
+    console.log(`JWS: Computing bundle value`)
+    let loc = gamesUrl(document.location)
+    if (!loc) {
+      if (loc.pathname.startswith("/s/")) {
+        return computeSalesBundleValue();
+      }
+      console.error("JWS: no game URL returned");
+      return;
+    }
+
+    console.log(`JWS: Fetching games.json from ${loc}`)
+    const res = await fetch(loc, { credentials: 'same-origin' })
+    if (!res.ok) {
+        console.error('JWS: failed fetching games.json')
+        return
+    }
+    const {games} = await res.json()
+    addPrices(games);
+  const unownedGames = games.filter((it) => !ownedGameIds.has(it.id));
+  console.log(
+    `JWS: Found ${games.length} total games, ${unownedGames.length} unowned games.`
+  );
+  const value = unownedGames.reduce((accu, game) => {
+    try {
+      if (game.flag === "free" || game.flag === "web") {
+        return accu;
+      }
+      const [key, value] = priceEntry(game.price);
+      if (!accu[key]) {
+        console.log(
+          `New currency symbol: ${key} - from price string: ${game.price}`
+        );
+        accu[key] = 0;
+      }
+      accu[key] += value;
+      //   console.log(`Now at ${key}${accu[key]}`);
+    } catch (error) {
+      console.error(`Failed to price a game.`, { game, error });
+    }
+    return accu;
+  }, {});
+  const msg = generateValueMessage(value, unownedGames.length);
   console.log(msg);
   done(msg)
   stickAmountOnPage(msg)
